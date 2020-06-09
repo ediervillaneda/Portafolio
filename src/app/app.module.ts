2d834bf798b9bae4c66370bb80f22d331fdef7bb
Edier Andrés Villaneda Navarro <ediervillaneda@gmail.com>
2020-06-08 21:26:56 -0500

    Implementación de Footer y Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    PortafolioComponent
   ],
   imports: [
     BrowserModule,
